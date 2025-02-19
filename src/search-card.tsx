79df2efa160e1d2a690b9a6f3eb4d39800df683c
Apope1999 <apezodromou@startmail.com>
2025-02-20 00:31:40 +0200

    Display results from User Input
    Displays the results in a list based on user input.

--- src/search-card.tsx
+++ src/search-card.tsx
@@ -1,23 +1,54 @@
+import { useState } from "react";
 import { ActionPanel, Detail, List, Action, Icon } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 
-export default function Command() {
+type Card = {
+  id: string;
+  name: string;
+  type: string;
+  desc: string;
+  atk: number;
+  def: number;
+  level: number;
+  attribute: string;
+  archetype: string;
+  ygoprodeck_url: string;
+}
 
-  const { isLoading, data, revalidate } = useFetch<SearchResponse>("https://db.ygoprodeck.com/api/v7/cardinfo.php?%20&fname=Pendulum")
+export default function Command() {
+  const [searchText, setSearchText] = useState("Blue-Eyes");
+  const { isLoading, data } = useFetch<Card[]>(`https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${searchText}`,
+  {
+    keepPreviousData: true,
+    parseResponse: async (response) => {
+      const json = await response.json();
+      return json.data;
+    },
+    mapResult: (result) => ({
+      data: result.map((card) => ({
+        id: card.id.toString(),
+        name: card.name,
+        type: card.type,
+        desc: card.desc,
+        atk: card.atk,
+        def: card.def,
+        level: card.level,
+        attribute: card.attribute,
+        archetype: card.archetype || "N/A",
+        ygoprodeck_url: card.ygoprodeck_url,
+      })),
+    }),
+  }
+);
+
 
   console.log(data)
 
   return (
-    <List>
-      <List.Item
-        icon={Icon.Bird}
-        title="Greeting"
-        actions={
-          <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
-      />
+    <List isLoading={isLoading} searchText={searchText} onSearchTextChange={setSearchText} throttle>
+      {data?.map((card) => (
+        <List.Item key={card.id} title={card.name} />
+      ))}
     </List>
   );
 }
