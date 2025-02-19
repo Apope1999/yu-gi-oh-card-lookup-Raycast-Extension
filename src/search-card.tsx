3c770b1bd766e051dae1907b9f03e952277090e0
Apope1999 <apezodromou@startmail.com>
2025-02-20 01:42:53 +0200

    Initial implementation
    TODO: Nothing works as of right now. This is a mockup template on how it should work.

--- src/search-card.tsx
+++ src/search-card.tsx
@@ -2,6 +2,30 @@ import { useState } from "react";
 import { ActionPanel, Detail, List, Action, Icon } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 
+type CardType = {
+  id: string;
+  name: string;
+}
+
+function CardTypeDropdown(props: { cardTypes: CardType[]; onCardTypeChange: (newValue: string) => void }) {
+  const { cardTypes, onCardTypeChange } = props;
+  return (
+    <List.Dropdown
+      tooltip="Select Card Type"
+      storeValue={true}
+      onChange={(newValue) => {
+        onCardTypeChange(newValue);
+      }}
+    >
+      <List.Dropdown.Section title="Cards">
+        {cardTypes.map((cardType) => (
+          <List.Dropdown.Item key={cardType.id} title={cardType.name} value={cardType.id} />
+        ))}
+      </List.Dropdown.Section>
+    </List.Dropdown>
+  );
+}
+
 type Card = {
   id: string;
   name: string;
@@ -16,6 +40,18 @@ type Card = {
 }
 
 export default function Command() {
+
+  const cardTypes: CardType[] = [
+    { id: "1", name: "All" },
+    { id: "2", name: "Monster" },
+    { id: "3", name: "Dark Magician" },
+  ];
+
+  const onCardTypeChange = (newValue: string) => {
+    console.log(newValue);
+  };
+
+
   const [searchText, setSearchText] = useState("");
   const { isLoading, data } = useFetch<Card[]>(`https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${searchText}`,
   {
@@ -46,7 +82,14 @@ export default function Command() {
   console.log(data)
 
   return (
-    <List isLoading={isLoading} searchBarPlaceholder={"Search Card..."} searchText={searchText} onSearchTextChange={setSearchText} throttle>
+    <List
+     isLoading={isLoading}
+     searchBarPlaceholder={"Search Card..."} 
+     searchText={searchText} 
+     onSearchTextChange={setSearchText} 
+     throttle
+     searchBarAccessory={<CardTypeDropdown cardTypes={cardTypes} onCardTypeChange={onCardTypeChange} />}
+     >
       {data?.map((card) => (
         <List.Item key={card.id} title={card.name} />
       ))}
