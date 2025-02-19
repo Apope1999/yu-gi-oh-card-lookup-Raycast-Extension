d04575f507ea96f5294a2a72e44551ff67dd6abc
Apope1999 <apezodromou@startmail.com>
2025-02-19 22:48:52 +0200

    Pull data w/ useFetch
    Pull data from API using useFetch and displaying them to console log.

--- src/search-card.tsx
+++ src/search-card.tsx
@@ -1,6 +1,12 @@
 import { ActionPanel, Detail, List, Action, Icon } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
 
 export default function Command() {
+
+  const { isLoading, data, revalidate } = useFetch<SearchResponse>("https://db.ygoprodeck.com/api/v7/cardinfo.php?%20&fname=Pendulum")
+
+  console.log(data)
+
   return (
     <List>
       <List.Item
