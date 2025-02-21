811a84638ed4a4b61590b1d748035dab477060bf
Apope1999 <apezodromou@startmail.com>
2025-02-21 22:28:24 +0200

    Fixed Formatting

--- src/search-card.tsx
+++ src/search-card.tsx
@@ -18,29 +18,29 @@ type Card = {
 export default function Command() {
   const [searchText, setSearchText] = useState("");
   const { isLoading, data } = useFetch<Card[]>(`https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${searchText}`,
-  {
-    keepPreviousData: true,
-    execute: searchText.trim().length > 3,
-    parseResponse: async (response) => {
-      const json = await response.json();
-      return json.data ?? [];
-    },
-    mapResult: (result) => ({
-      data: result.map((card) => ({
-        id: card.id.toString(),
-        name: card.name,
-        type: card.type,
-        desc: card.desc,
-        atk: card.atk,
-        def: card.def,
-        level: card.level,
-        attribute: card.attribute,
-        archetype: card.archetype || "N/A",
-        ygoprodeck_url: card.ygoprodeck_url,
-      })),
-    }),
-  }
-);
+    {
+      keepPreviousData: true,
+      execute: searchText.trim().length > 3,
+      parseResponse: async (response) => {
+        const json = await response.json();
+        return json.data ?? [];
+      },
+      mapResult: (result) => ({
+        data: result.map((card) => ({
+          id: card.id.toString(),
+          name: card.name,
+          type: card.type,
+          desc: card.desc,
+          atk: card.atk,
+          def: card.def,
+          level: card.level,
+          attribute: card.attribute,
+          archetype: card.archetype || "N/A",
+          ygoprodeck_url: card.ygoprodeck_url,
+        })),
+      }),
+    }
+  );
 
 
   console.log(data)
