0898f88ec2a89595a21888947e6e495f6bc0452f
Apope1999 <apezodromou@startmail.com>
2025-02-22 00:13:08 +0200

    Added all Dropdown Item

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -20,6 +20,35 @@ export function CardList({ isLoading, data, searchText, setSearchText }: any) {
       throttle
       searchBarAccessory={
         <List.Dropdown tooltip="Filter by Card Type" storeValue onChange={setSelectedType}>
+          <List.Dropdown.Item title="All" value="All" />
+          <List.Dropdown.Item title="Effect Monster" value="Effect Monster" />
+          <List.Dropdown.Item title="Flip Effect Monster" value="Flip Effect Monster" />
+          <List.Dropdown.Item title="Fusion Monster" value="Fusion Monster" />
+          <List.Dropdown.Item title="Gemini Monster" value="Gemini Monster" />
+          <List.Dropdown.Item title="Link Monster" value="Link Monster" />
+          <List.Dropdown.Item title="Normal Monster" value="Normal Monster" />
+          <List.Dropdown.Item title="Normal Tuner Monster" value="Normal Tuner Monster" />
+          <List.Dropdown.Item title="Pendulum Effect Fusion Monster" value="Pendulum Effect Fusion Monster" />
+          <List.Dropdown.Item title="Pendulum Effect Monster" value="Pendulum Effect Monster" />
+          <List.Dropdown.Item title="Pendulum Effect Ritual Monster" value="Pendulum Effect Ritual Monster" />
+          <List.Dropdown.Item title="Pendulum Flip Effect Monster" value="Pendulum Flip Effect Monster" />
+          <List.Dropdown.Item title="Pendulum Normal Monster" value="Pendulum Normal Monster" />
+          <List.Dropdown.Item title="Pendulum Tuner Effect Monster" value="Pendulum Tuner Effect Monster" />
+          <List.Dropdown.Item title="Ritual Effect Monster" value="Ritual Effect Monster" />
+          <List.Dropdown.Item title="Ritual Monster" value="Ritual Monster" />
+          <List.Dropdown.Item title="Skill Card" value="Skill Card" />
+          <List.Dropdown.Item title="Spell Card" value="Spell Card" />
+          <List.Dropdown.Item title="Spirit Monster" value="Spirit Monster" />
+          <List.Dropdown.Item title="Synchro Monster" value="Synchro Monster" />
+          <List.Dropdown.Item title="Synchro Pendulum Effect Monster" value="Synchro Pendulum Effect Monster" />
+          <List.Dropdown.Item title="Synchro Tuner Monster" value="Synchro Tuner Monster" />
+          <List.Dropdown.Item title="Token" value="Token" />
+          <List.Dropdown.Item title="Toon Monster" value="Toon Monster" />
+          <List.Dropdown.Item title="Trap Card" value="Trap Card" />
+          <List.Dropdown.Item title="Tuner Monster" value="Tuner Monster" />
+          <List.Dropdown.Item title="Union Effect Monster" value="Union Effect Monster" />
+          <List.Dropdown.Item title="XYZ Monster" value="XYZ Monster" />
+          <List.Dropdown.Item title="XYZ Pendulum Effect Monster" value="XYZ Pendulum Effect Monster" />
         </List.Dropdown>
       }
     >
