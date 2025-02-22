793df3bac6052420f92c6e141650d4f3443412f0
Apope1999 <apezodromou@startmail.com>
2025-02-22 12:33:55 +0200

    Updated card layout
    Included action to open the card in the browser on YGOProDeck

--- src/components/NormalMonsterDetail.tsx
+++ src/components/NormalMonsterDetail.tsx
@@ -1,9 +1,26 @@
-import { Detail } from "@raycast/api";
+import { Action, ActionPanel, Detail } from "@raycast/api";
 import { Card } from "../types/Card";
 
 export function NormalMonsterDetail({ card }: { card: Card }) {
     return (
         <Detail
+          markdown={`# ${card.name}\n## Card Text\n ${card.desc}`}
+          navigationTitle={`${card.name}`}
+          metadata={
+            <Detail.Metadata>
+              <Detail.Metadata.Label title="Attack" text={`${card.atk}`} />
+              <Detail.Metadata.Label title="Defense" text={`${card.def}`} />
+              <Detail.Metadata.Separator />
+              <Detail.Metadata.Label title="Level" text={`${card.level}`} />
+              <Detail.Metadata.Label title="Attribute" text={`${card.attribute}`} />
+              <Detail.Metadata.Label title="Archetype" text={`${card.archetype}`} />
+            </Detail.Metadata>
+          }
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser title="View in YGOProDeck" url={`${card.ygoprodeck_url}`} />
+            </ActionPanel>
+          }
         />
       );
 }
