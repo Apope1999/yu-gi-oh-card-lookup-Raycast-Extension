7bf3fd6d338ce43b777d250c35f325ac2d870591
Apope1999 <apezodromou@startmail.com>
2025-02-23 01:03:20 +0200

    Update Card.ts
    Added extra attributes fetched from the API

--- src/types/Card.ts
+++ src/types/Card.ts
@@ -1,12 +1,33 @@
+export type CardSet = {
+  set_name: string;
+  set_code: string;
+  set_rarity: string;
+  set_rarity_code: string;
+  set_price: string;
+};
+
+export type CardPrice = {
+  cardmarket_price: string;
+  tcgplayer_price: string;
+  ebay_price: string;
+  amazon_price: string;
+  coolstuffinc_price: string;
+};
+
 export type Card = {
-    id: string;
-    name: string;
-    type: string;
-    desc: string;
-    atk: number;
-    def: number;
-    level: number;
-    attribute: string;
-    archetype: string;
-    ygoprodeck_url: string;
-  };
+  id: string;
+  name: string;
+  typeline: string[];
+  type: string;
+  humanReadableCardType: string;
+  frameType: string;
+  desc: string;
+  atk: number;
+  def: number;
+  level: number;
+  attribute: string;
+  archetype: string;
+  ygoprodeck_url: string;
+  card_sets?: CardSet[];
+  card_prices?: CardPrice[];
+};
