503dbddd255696ffdc9117906315536f1287a2f0
Apope1999 <apezodromou@startmail.com>
2025-02-22 12:33:23 +0200

    Removed old card layout

--- src/components/NormalMonsterDetail.tsx
+++ src/components/NormalMonsterDetail.tsx
@@ -4,16 +4,6 @@ import { Card } from "../types/Card";
 export function NormalMonsterDetail({ card }: { card: Card }) {
     return (
         <Detail
-          markdown={`# ${card.name}
-      ${card.desc}  
-      **Type**: ${card.type}  
-      **ATK/DEF**: ${card.atk}/${card.def}  
-      **Level**: ${card.level}  
-      **Attribute**: ${card.attribute}  
-      **Archetype**: ${card.archetype}  
-      **[More Info](${card.ygoprodeck_url})**
-      ** Normal Monster **
-      `}
         />
       );
 }
