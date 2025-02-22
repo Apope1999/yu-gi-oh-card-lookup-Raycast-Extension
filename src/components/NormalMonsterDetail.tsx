024269b3384d7cc5c63a5e35a12a2c2df7da05ca
Apope1999 <apezodromou@startmail.com>
2025-02-23 01:06:23 +0200

    Updated markdown
    Redesigned the card view. Added the card price fetched from the API

--- src/components/NormalMonsterDetail.tsx
+++ src/components/NormalMonsterDetail.tsx
@@ -4,7 +4,12 @@ import { Card } from "../types/Card";
 export function NormalMonsterDetail({ card }: { card: Card }) {
     return (
         <Detail
-          markdown={`# ${card.name}\n## Card Text\n ${card.desc}`}
+          markdown={`# ${card.name}\n## Card Text\n ${card.desc}\n \n[${Array.isArray(card.typeline) ? card.typeline.map(type => `**${type}**`).join("/") : `**${card.typeline}**`}]\n *** \n ## Card Price\n ${card.card_prices!
+            .map(price => {
+                return `- **Cardmarket**: $${price.cardmarket_price}\n- **TCGPlayer**: $${price.tcgplayer_price}\n- **eBay**: $${price.ebay_price}\n- **Amazon**: $${price.amazon_price}\n- **CoolStuffInc**: $${price.coolstuffinc_price}
+                `.trim();
+            })
+            .join("\n")}`}
           navigationTitle={`${card.name}`}
           metadata={
             <Detail.Metadata>
