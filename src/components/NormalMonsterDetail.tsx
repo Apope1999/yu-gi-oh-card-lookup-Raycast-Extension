919f57472bec2690e3dc84470cdd00be1d626f70
Apope1999 <apezodromou@startmail.com>
2025-02-22 12:01:16 +0200

    Create NormalMonsterDetail.tsx
    Added the default definition for a Normal Monster

---
+++ src/components/NormalMonsterDetail.tsx
@@ -0,0 +1,19 @@
+import { Detail } from "@raycast/api";
+import { Card } from "../types/Card";
+
+export function NormalMonsterDetail({ card }: { card: Card }) {
+    return (
+        <Detail
+          markdown={`# ${card.name}
+      ${card.desc}  
+      **Type**: ${card.type}  
+      **ATK/DEF**: ${card.atk}/${card.def}  
+      **Level**: ${card.level}  
+      **Attribute**: ${card.attribute}  
+      **Archetype**: ${card.archetype}  
+      **[More Info](${card.ygoprodeck_url})**
+      ** Normal Monster **
+      `}
+        />
+      );
+}
